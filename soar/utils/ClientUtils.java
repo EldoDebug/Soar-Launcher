88021b80e0b7e825fa78c6545eaf857e9b625d4d
TheKodeToad <TheKodeToad@proton.me>
2022-06-27 10:21:50 +0100

    Fix launch on Linux

--- soar/utils/ClientUtils.java
+++ soar/utils/ClientUtils.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,40 +14,48 @@ import soar.utils.json.MinecraftLibrary;
 import soar.utils.json.MinecraftNativeLibrary;
 
 public class ClientUtils {
-	
+
     public static void launchClient(final List<MinecraftLibrary> libs) {
         try {
         	Soar.instance.setInfo("Launching...");
-            String launchCMD = "";
+            final List<String> launchCMD = new ArrayList<>();
             Soar.instance.logger.info("Launching Client...");
-            
+
             switch (OSType.getType()) {
 	            case WINDOWS:{
 	                Soar.instance.logger.info("Set Windows Java!");
-	                launchCMD = new File(FileUtils.launcherDir + File.separator + "java" + File.separator + "bin" + File.separator + "java").getPath();
+	                launchCMD.add(new File(FileUtils.launcherDir + "java/bin/java").getPath());
 	                break;
 	            }
 	            default:{
 	                Soar.instance.logger.info("Set OtherOS Java!");
-	                launchCMD = "java";
+	                launchCMD.add(System.getProperty("java.home", "/usr") + "/bin/java");
 	                break;
 	            }
             }
-            
-            launchCMD = launchCMD + " -Djava.library.path=\"" + FileUtils.nativesFolder.getAbsolutePath() + "\" -cp \"";
-            
+
+            launchCMD.add("-Djava.library.path=" + FileUtils.nativesFolder.getAbsolutePath());
+
+            final List<String> classpath = new ArrayList<>();
+
             for (final MinecraftLibrary lib : libs) {
                 final File libFile = new File(FileUtils.librariesFolder, lib.getPath());
-                launchCMD += libFile.getAbsolutePath();
-                launchCMD += File.pathSeparator;
+                classpath.add(libFile.getAbsolutePath());
             }
-            
-            launchCMD = launchCMD + FileUtils.clientFolder.getAbsolutePath() + File.separator + "*\" ";
-            launchCMD += "net.minecraft.client.main.Main ";
-            launchCMD += "--accessToken " + Soar.instance.getToken() + " ";
-            launchCMD += "--version SoarClient ";
-            launchCMD += "--username " + Soar.instance.getUsername() + " ";
-            launchCMD += "--uuid " + Soar.instance.getId() + " ";
+
+            classpath.add(FileUtils.clientFolder.getAbsolutePath() + "/*");
+
+            launchCMD.add("-cp");
+            launchCMD.add(String.join(File.pathSeparator, classpath));
+            launchCMD.add("net.minecraft.client.main.Main");
+            launchCMD.add("--accessToken");
+            launchCMD.add(Soar.instance.getToken());
+            launchCMD.add("--version");
+            launchCMD.add("SoarClient");
+            launchCMD.add("--username");
+            launchCMD.add(Soar.instance.getUsername());
+            launchCMD.add("--uuid");
+            launchCMD.add(Soar.instance.getId());
             String launchDirAfterUserFolder = null;
             switch (OSType.getType()) {
                 case WINDOWS: {
@@ -56,22 +65,26 @@ public class ClientUtils {
                 }
                 case MAC: {
 	                Soar.instance.logger.info("Set MAC Location!");
-                    launchDirAfterUserFolder = "Library\\Application Support\\.minecraft\\";
+                    launchDirAfterUserFolder = "Library/Application Support/.minecraft";
                     break;
                 }
                 default: {
 	                Soar.instance.logger.info("Set Other Location!");
-                    launchDirAfterUserFolder = "\\.minecraft\\";
+                    launchDirAfterUserFolder = ".minecraft";
                     break;
                 }
             }
-            
+
             final File launchF = new File(FileUtils.userDir, launchDirAfterUserFolder);
             final File assetsF = new File(launchF, "assets");
-            launchCMD = launchCMD + "--gameDir \"" + launchF.getAbsolutePath() + "\" ";
-            launchCMD = launchCMD + "--assetsDir \"" + assetsF.getAbsolutePath() + "\" ";
-            launchCMD += "--assetIndex 1.8 ";
-            final Process proc = Runtime.getRuntime().exec(launchCMD);
+            launchCMD.add("--gameDir");
+            launchCMD.add(launchF.getAbsolutePath());
+            launchCMD.add("--assetsDir");
+            launchCMD.add(assetsF.getAbsolutePath());
+            launchCMD.add("-assetIndex");
+            launchCMD.add("1.8");
+            System.out.println(launchCMD);
+            final Process proc = new ProcessBuilder(launchCMD).start();
             final BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
             String isLine;
             while ((isLine = reader.readLine()) != null) {
@@ -82,7 +95,7 @@ public class ClientUtils {
             while ((errLine = err.readLine()) != null) {
                 Soar.instance.logger.error(errLine);
             }
-            
+
             if (!proc.isAlive()) {
                 Soar.instance.setInfo("Launch");
             }
@@ -91,7 +104,7 @@ public class ClientUtils {
             e.printStackTrace();
         }
     }
-    
+
     public static void downloadNatives(final List<MinecraftNativeLibrary> nativeLibraries) {
         if (FileUtils.nativesFolder.listFiles().length == 0) {
     		Soar.instance.setInfo("Downloading...");
@@ -137,7 +150,7 @@ public class ClientUtils {
             }
         }
     }
-    
+
     public static void downloadLibraries(final List<MinecraftLibrary> libraries) {
         for (final MinecraftLibrary library : libraries) {
             Soar.instance.setInfo("Downloading...");
@@ -152,7 +165,7 @@ public class ClientUtils {
             }
         }
     }
-    
+
     public static void downloadJava() {
     	if(!FileUtils.javaFolder.exists()) {
     		Soar.instance.setInfo("Downloading...");
@@ -163,7 +176,7 @@ public class ClientUtils {
         	Soar.instance.logger.info("Success Download Java Files!");
     	}
     }
-    
+
     public static void downloadClient() {
     	Soar.instance.logger.info("Downloading Client Files...");
     	if(!(new File(FileUtils.clientFolder, "SoarClient.jar").exists()) || !(new File(FileUtils.clientFolder, "SoarClient.json").exists()) || ClientUtils.checkUpdate()) {
@@ -175,14 +188,14 @@ public class ClientUtils {
         	Soar.instance.logger.info("Success Download Client Files!");
     	}
     }
-    
+
     @SuppressWarnings("resource")
 	private static boolean checkUpdate() {
     	Soar.instance.logger.info("Checking Update...");
 		try {
 	        URL url = new URL("https://pastebin.com/raw/WG2RGHJ4");
 	        Scanner s = new Scanner(url.openStream());
-	        
+
 	        while (s.hasNext()) {
 	            String[] s2 = s.nextLine().split(":");
 	            if(!(new File(FileUtils.appdata, ".minecraft/soar/temp/" + s2[0] + ".ver").exists())) {
